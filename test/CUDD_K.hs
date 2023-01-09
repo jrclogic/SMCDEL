d104042e44fa21c39d9607fc44731bc5eb0e3a47
Malvin Gattinger <malvin@w4eg.de>
2023-01-09 13:28:50 +0100

    clean up MyHaskCUDD and test/CUDD_K

--- test/CUDD_K.hs
+++ test/CUDD_K.hs
@@ -8,21 +8,22 @@ import SMCDEL.Language
 import qualified SMCDEL.Symbolic.K_CUDD as Cudd
 import SMCDEL.Symbolic.K as Cac
 import SMCDEL.Symbolic.S5 as CacBase
-import qualified SMCDEL.Symbolic.S5_CUDD as CuddBase
+import SMCDEL.Symbolic.S5_CUDD (boolDdOf)
 import qualified SMCDEL.Internal.MyHaskCUDD as MyHaskCUDD (Manager, makeManagerZ)
-import SMCDEL.Internal.MyHaskCUDD (Dd, Z, B, F0, F1, S0, S1, DdCtx, top, restrictLaw, equ)
+import SMCDEL.Internal.MyHaskCUDD
 import qualified Data.Map.Strict as M
 
 main :: IO ()
 main = do
   mgr <- MyHaskCUDD.makeManagerZ 100 -- keep at 100 as BF randomises over 100 props
   hspec $ do
-    describe "hardcoded myScn" $ do
-      prop "all DD kinds" $ alleq . ddKindTest mgr
-      -- prop "all DD kinds on restrictLaw" (\x y -> alleq $ testRestrictlaw mgr (map P [0..100]) x y)
+    describe "hardcoded myKnS" $ do
+      prop "evalViaBdd agrees for all eight DD kinds" $ alleq . ddKindTest mgr
+    describe "CUDD tests" $ do
+      prop "restrictLaw is correct for all  " (\x y -> and $ testRestrictlaw mgr (map P [0..100]) x y)
 
 myKnS :: (DdCtx a b c) => MyHaskCUDD.Manager -> Cudd.BelStruct a b c
-myKnS mgr = Cudd.BlS mgr defaultVocabulary (CuddBase.boolDdOf mgr Top) $ myObservables (pure . CuddBase.boolDdOf mgr)
+myKnS mgr = Cudd.BlS mgr defaultVocabulary (boolDdOf mgr Top) (myObservables (pure . boolDdOf mgr))
 
 myKnScac :: Cac.BelStruct
 myKnScac = Cac.BlS defaultVocabulary (CacBase.boolBddOf Top) (myObservables (pure . boolBddOf))
@@ -37,29 +38,25 @@ myObservables formToDd = M.fromList
 
 ddKindTest :: MyHaskCUDD.Manager -> SimplifiedForm -> [Bool]
 ddKindTest mgr (SF f) =
-  [
-    Cac.evalViaBdd (myKnScac, defaultVocabulary) f,
-    Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene B F1 S1) f,
-    Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene B F1 S0) f,
-    Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene B F0 S1) f,
-    Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene B F0 S0) f,
-    Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene Z F1 S1) f,
-    Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene Z F1 S0) f,
-    Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene Z F0 S1) f,
-    Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene Z F0 S0) f
+  [ Cac.evalViaBdd (myKnScac, defaultVocabulary) f
+  , Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene B F1 S1) f
+  , Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene B F1 S0) f
+  , Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene B F0 S1) f
+  , Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene B F0 S0) f
+  , Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene Z F1 S1) f
+  , Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene Z F1 S0) f
+  , Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene Z F0 S1) f
+  , Cudd.evalViaDd ((myKnS mgr, defaultVocabulary) :: Cudd.BelScene Z F0 S0) f
   ]
 
-
 testRestrictlaw :: MyHaskCUDD.Manager -> [Prp] -> BF -> BF -> [Bool]
-testRestrictlaw mgr v (BF a) (BF b) = [
-      (equ mgr (restrictLaw mgr v (CuddBase.boolDdOf mgr a) (CuddBase.boolDdOf mgr b)) (CuddBase.boolDdOf mgr a) :: Dd B F1 S1) == (top mgr :: Dd B F1 S1),
-      (equ mgr (restrictLaw mgr v (CuddBase.boolDdOf mgr a) (CuddBase.boolDdOf mgr b)) (CuddBase.boolDdOf mgr a) :: Dd B F0 S1) == (top mgr :: Dd B F0 S1),
-      (equ mgr (restrictLaw mgr v (CuddBase.boolDdOf mgr a) (CuddBase.boolDdOf mgr b)) (CuddBase.boolDdOf mgr a) :: Dd B F1 S0) == (top mgr :: Dd B F1 S0),
-      (equ mgr (restrictLaw mgr v (CuddBase.boolDdOf mgr a) (CuddBase.boolDdOf mgr b)) (CuddBase.boolDdOf mgr a) :: Dd B F0 S0) == (top mgr :: Dd B F0 S0),
-      (equ mgr (restrictLaw mgr v (CuddBase.boolDdOf mgr a) (CuddBase.boolDdOf mgr b)) (CuddBase.boolDdOf mgr a) :: Dd Z F1 S1) == (top mgr :: Dd Z F1 S1),
-      (equ mgr (restrictLaw mgr v (CuddBase.boolDdOf mgr a) (CuddBase.boolDdOf mgr b)) (CuddBase.boolDdOf mgr a) :: Dd Z F0 S1) == (top mgr :: Dd Z F0 S1),
-      (equ mgr (restrictLaw mgr v (CuddBase.boolDdOf mgr a) (CuddBase.boolDdOf mgr b)) (CuddBase.boolDdOf mgr a) :: Dd Z F1 S0) == (top mgr :: Dd Z F1 S0),
-      (equ mgr (restrictLaw mgr v (CuddBase.boolDdOf mgr a) (CuddBase.boolDdOf mgr b)) (CuddBase.boolDdOf mgr a) :: Dd Z F0 S0) == (top mgr :: Dd Z F0 S0)  -- &&
-      ]
-
-  -- substit mgr 5 (CuddBase.boolDdOf mgr a) (CuddBase.boolDdOf mgr b) == ifthenelse mgr (CuddBase.boolDdOf mgr a) (restrict mgr (CuddBase.boolDdOf mgr b) (5,True)) (restrict mgr (CuddBase.boolDdOf mgr b) (5,False)) -- &&
+testRestrictlaw mgr v (BF a) (BF b) =
+  [ imp mgr (boolDdOf mgr b) (equ mgr (restrictLaw mgr v (boolDdOf mgr a) (boolDdOf mgr b)) (boolDdOf mgr a)) == (top mgr :: Dd B F1 S1)
+  , imp mgr (boolDdOf mgr b) (equ mgr (restrictLaw mgr v (boolDdOf mgr a) (boolDdOf mgr b)) (boolDdOf mgr a)) == (top mgr :: Dd B F0 S1)
+  , imp mgr (boolDdOf mgr b) (equ mgr (restrictLaw mgr v (boolDdOf mgr a) (boolDdOf mgr b)) (boolDdOf mgr a)) == (top mgr :: Dd B F1 S0)
+  , imp mgr (boolDdOf mgr b) (equ mgr (restrictLaw mgr v (boolDdOf mgr a) (boolDdOf mgr b)) (boolDdOf mgr a)) == (top mgr :: Dd B F0 S0)
+  , imp mgr (boolDdOf mgr b) (equ mgr (restrictLaw mgr v (boolDdOf mgr a) (boolDdOf mgr b)) (boolDdOf mgr a)) == (top mgr :: Dd Z F1 S1)
+  , imp mgr (boolDdOf mgr b) (equ mgr (restrictLaw mgr v (boolDdOf mgr a) (boolDdOf mgr b)) (boolDdOf mgr a)) == (top mgr :: Dd Z F0 S1)
+  , imp mgr (boolDdOf mgr b) (equ mgr (restrictLaw mgr v (boolDdOf mgr a) (boolDdOf mgr b)) (boolDdOf mgr a)) == (top mgr :: Dd Z F1 S0)
+  , imp mgr (boolDdOf mgr b) (equ mgr (restrictLaw mgr v (boolDdOf mgr a) (boolDdOf mgr b)) (boolDdOf mgr a)) == (top mgr :: Dd Z F0 S0)
+  ]
