17213fdceda43da92a5f877771807029b1cdefd1
Malvin Gattinger <malvin@w4eg.de>
2023-01-30 18:39:18 +0100

    use BDD-to-ZDD conversion to speed up SAP sizeExperiment

--- sizeExperiments/sumandproduct.hs
+++ sizeExperiments/sumandproduct.hs
@@ -9,27 +9,43 @@ import SMCDEL.Language
 import qualified SMCDEL.Symbolic.S5_CUDD as S5_CUDD
 import qualified SMCDEL.Symbolic.S5 as S5_CAC
 import qualified SMCDEL.Internal.MyHaskCUDD as MyHaskCUDD
-import SMCDEL.Internal.MyHaskCUDD (B, Z, O1, O0, I1, I0, DdCtx)
+import SMCDEL.Internal.MyHaskCUDD
 
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
-  gatherSizeData [60] -- TODO: getArgs
+  gatherSizeData [60,63,64,65,120] -- TODO: getArgs
 
 genSapSizesCac :: Int -> [Int]
 genSapSizesCac n = map (\(S5_CAC.KnS _ lawb _) -> S5_CAC.size lawb) $
   updateSequence (genSapKnStruct n) [ genSapForm1 n, genSapForm2 n, genSapForm3 n ]
 
-genSapSizesCudd ::  forall a b c . DdCtx a b c => Int -> IO [Int]
+genSapSizesCudd :: forall a b c . DdCtx a b c => Int -> IO [Int]
 genSapSizesCudd n = do
   start <- genSapKnStructCudd @a @b @c n -- also creates manager!
   return $ map (\(S5_CUDD.KnS mgr _ lawb _) -> MyHaskCUDD.size mgr lawb) $
     updateSequence start  [ genSapForm1 n, genSapForm2 n, genSapForm3 n ]
 
+genSapSizesCuddViaConvert :: forall b c . (Convert b c) => Int -> IO [Int]
+genSapSizesCuddViaConvert n = do
+  start <- genSapKnStructCudd @B @O1 @I1 n -- also creates manager!
+  let context = map fromEnum $ vocabOf start
+  return $ map (\(S5_CUDD.KnS mgr _ lawb _) -> MyHaskCUDD.size mgr (convertToZDD mgr context lawb :: Dd Z b c)) $
+    updateSequence start  [ genSapForm1 n, genSapForm2 n, genSapForm3 n ]
+
 gatherSizeData :: [Int] -> IO ()
 gatherSizeData ns = do
   putStrLn $ "Running SAP benchmark for ns=" ++ show ns ++ " and writing results to sap.dat ..."
-  writeFile "sap.dat" $ "Note that these results are formulated/grouped by their elimination rule labels, and not their Input/Output complement labels as we use in our program. The labels used in our program translate to their elimination rule equivalence as follows: B O1 I1 -> EQ (nodes with equal children are eliminated), Z O1 I1 -> T0 (nodes with THEN edges pointing towards 0 leaf node are removed), Z O1 I0 -> E0, Z O0 I1 -> T1, Z O1 I0 -> E1.\n\n" ++ firstLine ++ "\n"
+  writeFile "sap.dat" $
+    unlines [ "# Note: result columns are labelled with elimination"
+            , "# rules, not the input/output complement labels."
+            , "# The labels translate as follows:"
+            , "#   B O1 I1 -> EQ (nodes with equal children are eliminated)"
+            , "#   Z O1 I1 -> T0 (nodes with THEN edges to 0 leaf are removed)"
+            , "#   Z O1 I0 -> E0"
+            , "#   Z O0 I1 -> T1"
+            , "#   Z O1 I0 -> E1."
+            ] ++ firstLine ++ "\n"
   mapM_ linesFor ns
   putStrLn "Done."
   where
@@ -37,10 +53,16 @@ gatherSizeData ns = do
     variants =
       [ ("BDD", return . genSapSizesCac)
       , ("BDDc",  genSapSizesCudd @B @O1 @I1)
-      , ("ZO1I1", genSapSizesCudd @Z @O1 @I1)
-      , ("ZO1I0", genSapSizesCudd @Z @O1 @I0)
-      , ("ZO0I1", genSapSizesCudd @Z @O0 @I1)
-      , ("ZO0I0", genSapSizesCudd @Z @O0 @I0)
+      -- via conversion, fast:
+      , ("ZO1I1", genSapSizesCuddViaConvert @O1 @I1)
+      , ("ZO1I0", genSapSizesCuddViaConvert @O1 @I0)
+      , ("ZO0I1", genSapSizesCuddViaConvert @O0 @I1)
+      , ("ZO0I0", genSapSizesCuddViaConvert @O0 @I0)
+      -- directly on ZDDs, slow:
+      -- , ("ZO1I1", genSapSizesCudd @Z @O1 @I1)
+      -- , ("ZO1I0", genSapSizesCudd @Z @O1 @I0)
+      -- , ("ZO0I1", genSapSizesCudd @Z @O0 @I1)
+      -- , ("ZO0I0", genSapSizesCudd @Z @O0 @I0)
       ]
     linesFor n = do
       putStrLn $ "Running for n = " ++ show n
