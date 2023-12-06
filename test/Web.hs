80fa7bfe7c33863fecad40c76b9c348d034069e7
Malvin Gattinger <malvin@w4eg.de>
2023-12-06 20:38:54 +0100

    web: also test lex/parse/sanity errors

--- test/Web.hs
+++ test/Web.hs
@@ -12,20 +12,43 @@ import Test.Sandwich.WebDriver
 import Test.WebDriver
 
 spec :: TopSpec
-spec = introduceWebDriver wdOptions $
-  it "MuddyChildren examples gives expected output in web interface" $ withSession1 $ do
+spec = introduceWebDriver wdOptions $ do
+  it "MuddyChildren example yields expected result" $ withSession1 $ do
     openPage "http://127.0.0.1:3000/index.html"
     findElem (ByCSS "input[value='MuddyChildren']") >>= click
     findElem (ByCSS "#runbutton") >>= click
-    liftIO $ putStrLn "clicked run and waitng 4 second for MathJax"
-    liftIO $ threadDelay 4000000
-    liftIO $ putStrLn "continuing"
+    liftIO $ threadDelay 3000000 -- longer wait for MathJax
     output <- getText =<< findElem (ByCSS "#output")
     output `textShouldContain` "Oalice={p2,p3}"
     output `textShouldContain` expectedMcOutput
+  it "Empty input yields a parse error" $ withSession1 $ do
+    openPage "http://127.0.0.1:3000/index.html"
+    findElem (ByCSS "#runbutton") >>= click
+    liftIO $ threadDelay 1000000
+    output <- getText =<< findElem (ByCSS "#output")
+    output `textShouldContain` "Parse error in line 1, column 1"
+  it "Unknown symbols yield lexing error" $ withSession1 $ do
+    openPage "http://127.0.0.1:3000/index.html"
+    findElem (ByCSS ".ace_text-input") >>= clearInput
+    findElem (ByCSS ".ace_text-input") >>= sendKeys "@ $ %"
+    findElem (ByCSS "#runbutton") >>= click
+    liftIO $ threadDelay 1000000
+    output <- getText =<< findElem (ByCSS "#output")
+    output `textShouldContain` "Lex error"
+  it "Formula with unknown agent yields sanity error" $ withSession1 $ do
+    openPage "http://127.0.0.1:3000/index.html"
+    findElem (ByCSS ".ace_text-input") >>= clearInput
+    findElem (ByCSS ".ace_text-input") >>= sendKeys wrongAgentInput
+    findElem (ByCSS "#runbutton") >>= click
+    liftIO $ threadDelay 1000000
+    output <- getText =<< findElem (ByCSS "#output")
+    output `textShouldContain` "Sanity error: Query formula contains agents not in OBS"
 
 expectedMcOutput :: Text
 expectedMcOutput = "At which states is\n⟨!⋁{p1,p2,p3}⟩⋁{Kalice?p1,Kbob?p2,Kcarol?p3}\ntrue?\n{p1},{p2},{p3}\n"
+
+wrongAgentInput :: Text
+wrongAgentInput = "VARS 1,2 LAW Top OBS alice: 1 bob: 2 WHERE? charlie knows that 1"
 
 wdOptions :: WdOptions
 wdOptions = (defaultWdOptions "/tmp/tools") {
@@ -35,7 +58,6 @@ wdOptions = (defaultWdOptions "/tmp/tools") {
 
 main :: IO ()
 main = do
-  putStrLn "starting smcdel-web before running tests"
   withCreateProcess (proc "stack" ["exec", "smcdel-web"]) $
     \_ _ _ _ -> do
       (_, numFailures) <- runSandwich' Nothing defaultOptions spec
